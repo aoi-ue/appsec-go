14ea7042191abb2403f669909e1f6004fa79a7ea
yuhanyuhan <yuhanlian@gmail.com>
2024-08-15 19:34:02 +0800

    refactored: removed unnecessary code

--- portscanned/main.go
+++ portscanned/main.go
@@ -1,15 +1,13 @@
 package main
 
 import (
-	"appsec-go/http"
-	"appsec-go/scanner"
 	"fmt"
 )
 
 func main() {
 	fmt.Println("Sending request and scanning!!!")
 	go func() {
-	 	http.HTTPServer()
+	 	HTTPServer()
 	}()
-	scanner.Scanning()
+	Scanning()
 }
